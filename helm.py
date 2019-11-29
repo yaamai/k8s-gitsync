8c830e33864d7d380c9617d21bb5f9897ae33e03
yaamai <ya_amai@yahoo.co.jp>
2019-11-29 17:58:56 +0900

    Split dir walk utils

--- helm.py
+++ helm.py
@@ -5,6 +5,7 @@ import json
 from subprocess import PIPE
 from pprint import pprint as pp
 import hashlib
+import utils
 
 
 def _get_helm_values_hash(release_name):
@@ -34,44 +35,9 @@ def get_helm_state():
     pp(state)
 
 
-def get_deploy_config():
-    helm_deploy_config_pattern = re.compile(r'.*.helm')
-    helm_values_pattern = re.compile(r'.helm$')
-
-    def _get_helm_file(files):
-        ret = []
-        for f in files:
-            if helm_deploy_config_pattern.match(f):
-                ret.append(f)
-
-                for values_file_pattern in ['.values.yaml', '.values.yml']:
-                    values_file_path = helm_values_pattern.sub(values_file_pattern, f)
-                    if values_file_path in files:
-                        ret.append(values_file_path)
-        return ret
-
-    helm_deploy_config = {}
-    k8s_deploy_config = {}
-    for root, dirs, files in os.walk('repo'):
-        print(root, dirs, files)
-
-        helm_files = _get_helm_file(files)
-        if helm_files:
-            helm_deploy_config[root] = helm_files
-
-        k8s_files = set(files) - set(helm_files)
-        if k8s_files:
-            k8s_deploy_config[root] = k8s_files
-
-    print(helm_deploy_config)
-    print(k8s_deploy_config)
-
-    return k8s_deploy_config, helm_deploy_config
-
-
 def main():
     # get_helm_state()
-    get_deploy_config()
+    utils.get_deploy_config()
 
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,36 @@
+import re
+import os
+
+def get_deploy_config():
+    helm_deploy_config_pattern = re.compile(r'.*.helm')
+    helm_values_pattern = re.compile(r'.helm$')
+
+    def _get_helm_file(files):
+        ret = []
+        for f in files:
+            if helm_deploy_config_pattern.match(f):
+                ret.append(f)
+
+                for values_file_pattern in ['.values.yaml', '.values.yml']:
+                    values_file_path = helm_values_pattern.sub(values_file_pattern, f)
+                    if values_file_path in files:
+                        ret.append(values_file_path)
+        return ret
+
+    helm_deploy_config = {}
+    k8s_deploy_config = {}
+    for root, dirs, files in os.walk('repo'):
+        print(root, dirs, files)
+
+        helm_files = _get_helm_file(files)
+        if helm_files:
+            helm_deploy_config[root] = helm_files
+
+        k8s_files = set(files) - set(helm_files)
+        if k8s_files:
+            k8s_deploy_config[root] = k8s_files
+
+    print(helm_deploy_config)
+    print(k8s_deploy_config)
+
+    return k8s_deploy_config, helm_deploy_config
