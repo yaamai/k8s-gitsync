0b7658b47cb63170ee7e077c8a757508602f8e03
rsp9u <gctwt852@yahoo.co.jp>
2019-12-01 16:57:50 +0900

    Impl cleaning removed k8s resources to work well

--- k8s.py
+++ k8s.py
@@ -8,7 +8,7 @@ import utils
 logger = log.getLogger(__name__)
 
 LAST_APPLIED_KEY = "k8s-gitsync/last-applied-confighash"
-KGS_LABEL = "k8s-gitsync/mamaged"
+KGS_MANAGED_KEY = "k8s-gitsync/managed"
 
 
 def _ensure_namespace(namespace):
@@ -44,10 +44,14 @@ def _apply_manifest(manifest, filehash):
         manifest["metadata"]["annotations"] = {}
     manifest["metadata"]["annotations"][LAST_APPLIED_KEY] = filehash
 
+    if manifest["metadata"].get("labels") is None:
+        manifest["metadata"]["labels"] = {}
+    manifest["metadata"]["labels"][KGS_MANAGED_KEY] = "true"
+
     _ensure_namespace(manifest["metadata"]["namespace"])
 
     cmd = ["kubectl", "apply", "-f", "-"]
-    _, errs, _ = utils.cmd_exec(cmd)
+    _, errs, _ = utils.cmd_exec(cmd, stdin=yaml.dump(manifest).encode())
     if errs:
         logger.error(f"failed to execute kubectl apply, {errs}")
 
@@ -64,27 +68,68 @@ def create_or_update(filepath):
     logger.info(f"applied {manifest['metadata']}")
 
 
+def _k8s_resource_id(metadata):
+    return f'{metadata["namespace"]}.{metadata["name"]}'
+
+
 def destroy_unless_exist_in(manifest_filepaths):
     manifest_ids_per_kind = defaultdict(list)
     for filepath in manifest_filepaths:
-        m, _ = _parse_manifest_file(filepath)
-        manifest_id = f'{m["matadata"]["namespace"]}.{m["metadata"]["name"]}'
-        manifest_ids_per_kind[m["kind"]].append(manifest_id)
+        manifest, _ = _parse_manifest_file(filepath)
+        manifest_ids_per_kind[manifest["kind"].lower()].append(_k8s_resource_id(manifest["metadata"]))
+    logger.info(f"existing manifests: {dict(manifest_ids_per_kind)}")
 
-    cmd = ["kubectl", "api-resources"]
+    cmd = ["kubectl", "api-resources", "-o", "name"]
     outs, _, _ = utils.cmd_exec(cmd)
     kinds = outs.decode().split()
 
     for kind in kinds:
-        cmd = ["kubectl", "--all-namespaces", "-l", KGS_LABEL+"=true", "get", kind, "-o", "json"]
-        outs, _, _ = utils.cmd_exec(cmd)
-        states = json.loads(outs.decode())["items"]
+        cmd = ["kubectl", "get", kind, "--all-namespaces", "-l", KGS_MANAGED_KEY + "=true", "-o", "json"]
+        outs, errs, _ = utils.cmd_exec(cmd)
+        if errs:
+            if "NotFound" in errs.decode() or "MethodNotAllowed" in errs.decode():
+                logger.debug(f"k8s returned an ignorable error against retrieving states of {kind}")
+            else:
+                logger.error(f"failed to get k8s states: {errs.decode()}")
+            continue
 
-        for s in states:
-            state_id = f'{s["matadata"]["namespace"]}.{s["metadata"]["name"]}'
-            if state_id not in manifest_ids_per_kind[kind]:
+        states = json.loads(outs.decode())["items"]
+        states = _filter_states_by_label(states, KGS_MANAGED_KEY, "true")
+        if len(states) != 0:
+            logger.info(f'existing states[{kind}]: {[_k8s_resource_id(s["metadata"]) for s in states]}')
+
+        for state in states:
+            state_id = _k8s_resource_id(state["metadata"])
+            if state_id not in manifest_ids_per_kind[state["kind"].lower()]:
+                logger.info(f"{state_id} does not exist, it will be destroyed")
                 _delete_state(state)
+            else:
+                logger.debug(f"{state_id} exists")
+
+
+def _filter_states_by_label(states, labelkey, labelvalue):
+    # NOTE:
+    # 'kubectl api-resources' may return some kinds that does not work for Selector (e.g. 'componentstatuses'),
+    # so k8s resources must be checked with the label.
+    def _(state):
+        labels = state["metadata"].get("labels", {})
+        logger.debug(f"labels: {labels}")
+        value = labels.get(labelkey, None)
+        if value is None:
+            return False
+        logger.debug(f"found; {labelkey}: {value}")
+        if value != labelvalue:
+            return False
+        return True
+
+    return list(filter(_, states))
 
 
 def _delete_state(state):
-    pass
+    resource_id = _k8s_resource_id(state["metadata"])
+    logger.info(f"deleting {resource_id}")
+    cmd = ["kubectl", "-n", state["metadata"]["namespace"], "delete", state["kind"], state["metadata"]["name"]]
+    _, errs, _ = utils.cmd_exec(cmd)
+    if errs:
+        logger.error(f"failed to delete {resource_id}: {errs.decode()}")
+    logger.info(f"deleted {resource_id}")
--- main.py
+++ main.py
@@ -8,13 +8,22 @@ def main():
     cli_desc = "Synchronizing the states with the manifests of k8s/helm"
     parser = argparse.ArgumentParser(description=cli_desc)
     parser.add_argument("repo", help="manifests repository directory")
+    parser.add_argument("--clean", action="store_true", help="clean up the resources removed from manifests")
     conf = parser.parse_args()
 
     k8s_manifests, helm_manifests = utils.get_manifest_files(conf.repo)
 
-    for manifest_dir, manifest_files in k8s_manifests.items():
-        for manifest_file in manifest_files:
-            k8s.create_or_update(os.path.join(manifest_dir, manifest_file))
+    if not conf.clean:
+        for manifest_dir, manifest_files in k8s_manifests.items():
+            for manifest_file in manifest_files:
+                k8s.create_or_update(os.path.join(manifest_dir, manifest_file))
+    else:
+        filepaths = []
+        for manifest_dir, manifest_files in k8s_manifests.items():
+            for manifest_file in manifest_files:
+                filepaths.append(os.path.join(manifest_dir, manifest_file))
+
+        k8s.destroy_unless_exist_in(filepaths)
 
 
 if __name__ == "__main__":
