44f817ef4355e7200d340e6da1574a1364551ceb
yaamai <ya_amai@yahoo.co.jp>
2019-12-12 08:14:29 +0000

    Fix namespace-less resource handling

--- k8s_gitsync/k8s.py
+++ k8s_gitsync/k8s.py
@@ -10,6 +10,7 @@ logger = log.getLogger(__name__)
 LAST_APPLIED_KEY = "k8s-gitsync/last-applied-confighash"
 KGS_MANAGED_KEY = "k8s-gitsync/managed"
 KGS_REQUIRES_KEY = "k8s-gitsync/requires"
+KGS_DEFAULT_NS = "default"
 
 
 def _ensure_namespace(namespace):
@@ -18,7 +19,7 @@ def _ensure_namespace(namespace):
 
 
 def _get_state(manifest):
-    namespace = manifest["metadata"]["namespace"]
+    namespace = manifest["metadata"].get("namespace", KGS_DEFAULT_NS)
     name = manifest["metadata"]["name"]
     kind = manifest["kind"]
 
@@ -42,7 +43,7 @@ def _apply_manifest(manifest, filehash):
         manifest["metadata"]["labels"] = {}
     manifest["metadata"]["labels"][KGS_MANAGED_KEY] = "true"
 
-    _ensure_namespace(manifest["metadata"]["namespace"])
+    _ensure_namespace(manifest["metadata"].get("namespace", KGS_DEFAULT_NS))
 
     cmd = ["kubectl", "apply", "-f", "-"]
     _, errs, _ = utils.cmd_exec(cmd, stdin=yaml.dump(manifest).encode())
@@ -83,7 +84,7 @@ def create_or_update(resource, is_dry_run):
 
 
 def _k8s_resource_id(kind, metadata):
-    return f'{kind.lower()}.{metadata["namespace"]}.{metadata["name"]}'
+    return f'{kind.lower()}.{metadata.get("namespace", KGS_DEFAULT_NS)}.{metadata["name"]}'
 
 
 def destroy_unless_exist_in(resources, is_dry_run):
@@ -142,7 +143,8 @@ def _filter_states_by_label(states, labelkey, labelvalue):
 def _delete_state(state):
     resource_id = _k8s_resource_id(state["kind"], state["metadata"])
     logger.info(f"deleting {resource_id}")
-    cmd = ["kubectl", "-n", state["metadata"]["namespace"], "delete", state["kind"], state["metadata"]["name"]]
+    namespace = state["metadata"].get("namespace", KGS_DEFAULT_NS)
+    cmd = ["kubectl", "-n", namespace, "delete", state["kind"], state["metadata"]["name"]]
     _, errs, _ = utils.cmd_exec(cmd)
     if errs:
         logger.error(f"failed to delete {resource_id}: {errs.decode()}")
