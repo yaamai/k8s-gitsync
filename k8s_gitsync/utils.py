b50fa7621e818d007e2749a2694c4fdb6e801688
yaamai <ya_amai@yahoo.co.jp>
2019-12-12 08:15:54 +0000

    Refactor target files serach

--- k8s_gitsync/utils.py
+++ k8s_gitsync/utils.py
@@ -1,6 +1,6 @@
 import re
 import os
-from copy import deepcopy
+from glob import glob
 from subprocess import Popen, PIPE
 from .resource import Resource
 from . import log
@@ -13,40 +13,34 @@ def get_manifest_files(repo_dir):
     helm_values_pattern = re.compile(r"(.*)\.values\.ya?ml$")
     k8s_pattern = re.compile(r"(.*)\.ya?ml$")
 
-    def _get_helm_file(root, files):
-        helm_files = []
-        helm_files_flat = []
-        match_list = [(helm_manifest_pattern.match(f), helm_values_pattern.match(f), f) for f in files]
+    def _get_helm_file(files):
+        manifest_list = []
+        helm_match_list = [(f, helm_manifest_pattern.match(f)) for f in files if helm_manifest_pattern.match(f)]
+        values_match_list = [(f, helm_values_pattern.match(f)) for f in files if helm_values_pattern.match(f)]
+        for filename, helm_match in helm_match_list:
+            d = Resource("helm", filename)
+            d.values = [f for f, m in values_match_list if helm_match.group(1) == m.group(1)]
+            files.remove(filename)
+            for e in d.values:
+                files.remove(e)
+            manifest_list.append(d)
+        return files, manifest_list
 
-        for helm_match, values_match, filename in match_list:
-            if helm_match:
-                d = Resource("helm", os.path.join(root, filename))
-                d.values = [
-                    os.path.join(root, f)
-                    for (_, m, f) in match_list
-                    if m is not None and helm_match.group(1) == m.group(1)
-                ]
-                helm_files.append(d)
-                helm_files_flat.append(d["manifest"])
-                helm_files_flat.extend(d["values"])
+    def _get_k8s_file(files):
+        manifest_list = []
+        k8s_files = filter(k8s_pattern.match, files)
+        manifest_list.extend([Resource("k8s", f) for f in k8s_files])
+        for e in k8s_files:
+            files.remove(e)
+        return files, manifest_list
 
-        return helm_files_flat, helm_files
+    path = os.path.join(repo_dir, "**/*")
+    logger.info(f"begin to walk manifest from {path}")
+    files = glob(path, recursive=True)
+    logger.info(f"  target files: {files}")
 
-    def _get_k8s_file(root, files):
-        k8s_files = [f for f in files if k8s_pattern.match(f)]
-        return [Resource("k8s", os.path.join(root, f)) for f in k8s_files]
-
-    logger.info("begin to walk manifest directory.")
-    helm_manifest = []
-    k8s_manifest = []
-    for root, dirs, files in os.walk(repo_dir):
-        logger.debug(f"{root}, {dirs}, {files}")
-
-        helm_files_flat, helm_files = _get_helm_file(root, files)
-        helm_manifest.extend(helm_files)
-        files = set(files) - set(helm_files_flat)
-
-        k8s_manifest.extend(_get_k8s_file(root, files))
+    files[:], helm_manifest = _get_helm_file(files)
+    files[:], k8s_manifest = _get_k8s_file(files)
 
     logger.info(f"detected k8s manifest files: {k8s_manifest}")
     logger.info(f"detected helm manifest files: {helm_manifest}")
