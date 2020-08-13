2a3ae6305804629992cd7f540551b8d0b171872e
yaamai <ya_amai@yahoo.co.jp>
2020-08-13 00:39:18 +0000

    Refactor result

--- kgs.py
+++ kgs.py
@@ -4,6 +4,7 @@ import re
 from collections import OrderedDict
 from dataclasses import dataclass
 from dataclasses import field
+from enum import Enum
 from pathlib import Path
 from subprocess import PIPE
 from subprocess import Popen
@@ -13,6 +14,7 @@ from typing import Generic
 from typing import Iterable
 from typing import List
 from typing import Optional
+from typing import Tuple
 from typing import Type
 from typing import TypeVar
 
@@ -149,28 +151,45 @@ class K8SState:
 T = TypeVar("T")
 
 
+class ResultKind(Enum):
+    unknown = "unknown"
+    notfound = "notfound"
+
+
+@dataclass
 class Result(Generic[T]):
-    def __init__(self, result: T, detail: dict = None):
-        self.result = result
-        self.detail = detail
-
-    def get(self) -> T:
-        if self.result is not None:
-            return self.result
-        return self
-
-    def __bool__(self) -> bool:
-        return self.result is not None
+    result: Optional[T]
+    detail: Optional[dict]
+    is_err: bool
+    kind: ResultKind = ResultKind.unknown
 
     @staticmethod
-    def error(detail: dict):
-        return Result[T](None, detail)  # type:ignore
+    def ok(result: T, detail: Optional[dict] = None) -> "Result[T]":
+        return Result[T](result, detail, False)
+
+    @staticmethod
+    def err(detail: dict, kind: ResultKind = ResultKind.unknown) -> "Result[T]":
+        return Result[T](None, detail, True, kind)
 
     @staticmethod
     def chain(result: "Result[T]"):
-        return Result[T](None, result.detail)  # type:ignore
+        return Result[T](None, result.detail, result.is_err, result.kind)
+
+    # unwrap result and check err
+    def chk(self, *args: ResultKind) -> Tuple[T, "Result[T]", List[bool]]:
+        kind_chk_result = [self.kind == k for k in args]
+        return self.result, self, [self.is_err and not any(kind_chk_result)] + kind_chk_result  # type:ignore
 
 
+# T, None: on success
+# T, {}: on success with additional info
+# None, None: on failure with no additional info
+# None, {}: on failure with additional info
+# T, T: ?
+# None, T: ?
+# {}, None: ?
+# {}, {}: ?
+# {}, T: ?
 class K8SOperator:
     @staticmethod
     def get_state(manifest: K8SManifest) -> Result[K8SState]:
@@ -178,43 +197,40 @@ class K8SOperator:
         name = manifest.get_name()
         kind = manifest.get_kind()
         if not all([namespace, name, kind]):
-            return Result.error({"msg": "invalid manifest"})
+            return Result.err({"msg": "invalid manifest"})
 
         cmd = ["kubectl", "-n", namespace, "get", kind, name, "-o", "json"]
         outs, errs, rc = cmd_exec(cmd)
         if ("(NotFound):" in errs.decode()) and rc != 0:
-            return Result(K8SState(m=manifest, state={}))
-        if rc != 0:
-            return Result.error({"msg": "unexpected return code", "raw": errs.decode()})
+            return Result.err({"msg": "state not found"}, ResultKind.notfound)
 
-        return Result(K8SState(m=manifest, state=json.loads(outs.decode())))
+        if rc != 0:
+            return Result.err({"msg": "unexpected return code", "raw": errs.decode()})
+
+        return Result.ok(K8SState(m=manifest, state=json.loads(outs.decode())))
 
     @staticmethod
     def _ensure_namespace(namespace):
         cmd = ["kubectl", "create", "namespace", namespace]
-        cmd_exec(cmd)
+        return cmd_exec(cmd)
 
-    def create_or_update(self, manifest: K8SManifest, dry_run: bool) -> Result[dict]:
-        result = self.get_state(manifest)
-        if not (state := result.get()):  # pylint: disable=superfluous-parens
-            return Result.chain(result)
+    def create_or_update(self, manifest: K8SManifest, dry_run: bool):
+        state, _, [is_err, notfound] = self.get_state(manifest).chk(ResultKind.notfound)
+        if is_err:
+            return
 
-        if state.is_updated():
-            return Result({"updated": True})
+        if not notfound and state.is_updated():
+            return
 
         if dry_run:
-            return Result({"dry_run": True})
+            return
 
         self._ensure_namespace(manifest.get_namespace())
 
         cmd = ["kubectl", "apply", "-f", "-"]
         _, _, _ = cmd_exec(cmd, stdin=yaml.dump(manifest.data).encode())
 
-        return Result({})
-
-
-class HelmClient:
-    pass
+        return
 
 
 @dataclass_json
@@ -261,13 +277,13 @@ class HelmOperator:
         values = json.loads(outs.decode())
 
         if ("release: not found" in errs.decode()) and rc != 0:
-            return Result({})
+            return Result.err({"msg": "notfound"}, ResultKind.notfound)
         if values is None:
-            return Result({})
+            return Result.err({"msg": "unknown"})
         if rc != 0:
-            return Result.error({"msg": "unexpected return code", "raw": errs.decode()})
+            return Result.err({"msg": "unexpected return code", "raw": errs.decode()})
 
-        return Result(values)
+        return Result.ok(values)
 
     def get_state(self, manifest: HelmManifest) -> Result[HelmState]:
         breakpoint()
@@ -275,16 +291,17 @@ class HelmOperator:
         name = manifest.get_name()
         chart = manifest.get_chart()
 
-        result = self.get_values(namespace, name)
-        if not (values := result.get()):  # pylint: disable=superfluous-parens
-            return Result.chain(result)
+        values, ret, [is_err] = self.get_values(namespace, name).chk()
+        if is_err:
+            return Result.chain(ret)
+
         state = {
             "release_name": manifest.get_name(),
             "chart": chart,
             "namespace": namespace,
             "_values_data": values,
         }
-        return Result(HelmState(m=manifest, state=state))
+        return Result.ok(HelmState(m=manifest, state=state))
 
 
 def _get_files_in_samedir(paths: Iterable[Path], filepath: Path, condition: Callable[[str], bool]) -> List[Path]:
