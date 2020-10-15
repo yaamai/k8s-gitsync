a0de31e7b3a8a2a4f55339363b00e354fc96bcf4
yaamai <ya_amai@yahoo.co.jp>
2020-10-15 05:15:51 +0000

    fix packaging

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
+from setuptools import find_packages
 from setuptools import setup
 
 setup(
     name="kgs",
     version="0.0.1",
-    install_requires=["PyYAML", "toposort"],
-    packages=["kgs"],
-    entry_points={"console_scripts": ["kgs = kgs.main:main",]},
+    install_requires=["PyYAML", "toposort", "typing-extensions"],
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "kgs = kgs.main:main",
+        ]
+    },
 )
